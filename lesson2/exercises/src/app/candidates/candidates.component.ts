0a61c22dbe6cc17df57105f7e30b969a996f3941
DreamHigherVisions <dreamhighervisions@DreamHigherVisions.local>
2019-10-27 09:04:02 -0500

    Updated example solutions and completed exercises for lesson2

--- lesson2/exercises/src/app/candidates/candidates.component.ts
+++ lesson2/exercises/src/app/candidates/candidates.component.ts
@@ -24,9 +24,14 @@ export class CandidatesComponent implements OnInit {
   }
 
   // Code the addToCrew function here:
-
+  addToCrew(candidate) {
+    if (!this.crew.includes(candidate.name)){
+      this.crew.push(candidate.name)
+    }
+  }
 
   // BONUS: Code the changeMissionName function here:
-
-
+  changeMissionName(newName){
+    this.missionName = newName
+  }
 }
